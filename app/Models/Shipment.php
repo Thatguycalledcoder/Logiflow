cf75ebab9835384a5e76c0efed79cc68a80fa5b7
Thatguycalledcoder <quarshiedavid62@gmail.com>
2026-07-16 22:09:11 +0000

    Added fillable fields for Shipment model.

--- app/Models/Shipment.php
+++ app/Models/Shipment.php
@@ -9,4 +9,14 @@ class Shipment extends Model
 {
     /** @use HasFactory<\Database\Factories\ShipmentFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'tracking_number',
+        'status',
+        'origin_address',
+        'destination_address',
+        'weight',
+        'price',
+        'driver_id'
+    ];
 }
