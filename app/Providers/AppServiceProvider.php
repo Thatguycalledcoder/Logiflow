4987a1582e13d108f62263f27d2561587ce11419
Thatguycalledcoder <quarshiedavid62@gmail.com>
2026-07-23 00:06:57 +0000

    Registered event listeners.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Events\ShipmentStatusUpdated;
+use App\Listeners\SendCustomerShipmentNotification;
+use App\Listeners\SendDriverPushNotification;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(
+            ShipmentStatusUpdated::class,
+            SendCustomerShipmentNotification::class,
+        );
+
+        Event::listen(
+            ShipmentStatusUpdated::class,
+            SendDriverPushNotification::class,
+        );
     }
 }
